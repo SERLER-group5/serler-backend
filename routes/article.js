dc81875e184e791c4e82d8994f5be197e4bf205e
frank-blueboat <tgzhsz@gmail.com>
2019-10-14 09:37:00 +1300

    Update article.js
    modify the returned fields of the queried results of the articles

--- routes/article.js
+++ routes/article.js
@@ -1,89 +1,45 @@
-const _ = require('lodash');
-const express = require('express');
-const router = express.Router();
-const {Article, validate} = require('../models/article');
-const {Status} = require('../models/status');
-const { Role } = require('../models/role');
-const { User } = require('../models/user');
-const auth = require('../middleware/authenticator');
-const admin = require('../middleware/admin');
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const moment = require('moment');
 
-/**
- * article router
- */
-router.get('/', async(req, res)=>{
-    const articles = await Article.find();
-    console.log('all articles are: ', articles);
-    res.send(articles);
+const analystSchema = new mongoose.Schema({
+    name: {type: mongoose.Types.ObjectId, ref: "User"},
+    analysedDate: {type: Date, default: Date, get: v => moment(v).format('MMMM Do YYYY, h:mm:ss a')}
 });
 
-router.get('/:id', async(req, res)=>{
-    const article = await Article.findById(req.params.id);
-    console.log('the article of %s is: ', req.params.id, article);
-    if (!article) {
-        return res.status(404).send('The article with given id was not found');
-    }
-    res.send(article);
+const moderatorSchema = new mongoose.Schema({
+    name: {type: mongoose.Types.ObjectId, ref: "User"},
+    moderatedDate: {type: Date, default: Date, get: v => moment(v).format('MMMM Do YYYY, h:mm:ss a')}
 });
 
-router.post('/', async(req, res)=>{
-    console.log('newly added article is: ', req.body);
-    const validatedFields = {
-        title: req.body.title,
-        submitter: req.body.submitter
+const rejectorSchema = new mongoose.Schema({
+    name: {type: mongoose.Types.ObjectId, ref: "User"},
+    rejectedDate: {type: Date, default: Date, get: v => moment(v).format('MMMM Do YYYY, h:mm:ss a')}
+});
+
+const articleSchema = new mongoose.Schema({
+    title: {type:String, required:true, minlength:5, maxlength:255},
+    content: {type:String, trim:true},
+    submitter: {type: mongoose.Types.ObjectId, ref: "User",  required: true},
+    tags: [ {type:String, trim:true, minlength:2, maxlength:255} ],
+    status: {type: mongoose.Types.ObjectId, ref: "Status"},
+    moderator: {type: moderatorSchema},
+    analyst: {type: analystSchema},
+    rejector: {type: rejectorSchema},
+    createdDate: {type: Date, default: Date, get: v => moment(v).format('MMMM Do YYYY, h:mm:ss a')},
+    updatedDate: {type: Date, default: Date, get: v => moment(v).format('MMMM Do YYYY, h:mm:ss a')}
+});
+
+function validateArticle(article){
+    const schema = {
+        title: Joi.string().min(3).required(),
+        submitter: Joi.string().required()
     };
-    const { error } = validate(validatedFields);
-    if (error) {
-        console.log('error information: ', error.details[0].message);
-        return res.status(400).send(error.details[0].message);
-    }
+    return Joi.validate(article, schema);
+}
 
-    const sumitter = await User.findById(req.body.submitter);
-    if (!sumitter) {
-        return res.status(400).send('Not found the submitter of the current article.');
-    }
+const Article = mongoose.model('Article', articleSchema);
 
-    const submittedStatus = await Status.findOne({name: 'submitted'});
-    if (!submittedStatus) {
-        return res.status(400).send('Has no status data for article to choose');
-    }
-
-    // get the tags string and split with the comma
-    let tagStr = req.body.tags;
-    let tags = [];
-    if (tagStr) {
-        tags = tagStr.split(';');
-    }
-    article = new Article(_.pick(req.body, ['title', 'content', 'submitter']));
-    article.status = submittedStatus._id;
-    article.tags = tags;
-    await article.save();
-
-    res.send(article);
-});
-
-router.put('/:id', async(req, res)=>{
-    let updatedStatus = req.body;
-    try{
-        let updatedRs = await Article.findByIdAndUpdate(req.params.id, updatedStatus,{new:true});
-        if(!updatedRs) {
-            return res.status(404).send('The article with given id was not found');
-        }
-        res.send(updatedRs);
-    }
-    catch(err)
-    {
-        console.error(err);
-    }
-});
-
-router.delete('/:id', async(req, res)=>{
-    const removedArticle = await Article.findByIdAndRemove(req.params.id);
-    if(!removedArticle) {
-        return res.status(404).send('The article with given id was not found');
-    }
-    res.send(removedArticle);
-});
-
-
- module.exports=router;
+exports.articleSchema = articleSchema;
+exports.Article = Article;
+exports.validate = validateArticle;
